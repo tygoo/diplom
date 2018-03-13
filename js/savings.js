062b9c6edd351cf1da4f3d707c98b6f6bc3ab824
tygoo <37063996+tygoo@users.noreply.github.com>
2018-03-13 16:17:34 +0800

    web platform for 3d Integral Imaging display
    send data x, y ,z and traditionally or fast method to server folder

---
+++ js/savings.js
@@ -0,0 +1,58 @@
+var cnv = document.getElementById('sketchpad');  //Replace 'cnv1' with your canvas ID
+var php_file ='save_cnvimg.php';  //address of php file that get and save image on server
+var axis_data;
+var zAxis;
+/* Ajax Function
+ Send "data" to "php", using the method added to "via", and pass response to "callback" function
+ data - object with data to send, name:value; ex.: {"name1":"val1", "name2":"2"}
+ php - address of the php file where data is send
+ via - request method, a string: 'post', or 'get'
+ callback - function called to proccess the server response
+*/
+function ajaxSend(data, php, via, callback) {
+  var ob_ajax = new XMLHttpRequest();
+
+  //put data from 'data' into a string to be send to 'php'
+  var str_data ='';
+  for(var k in data) {
+    str_data += k +'='+ data[k] + '&';
+  }
+  str_data = str_data.replace(/&$/, '');  //delete ending &sen
+  //send data to php
+  ob_ajax.open(via, php, true);
+  if(via =='post') ob_ajax.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  ob_ajax.send(str_data);
+
+  //check the state request, if completed, pass the response to callback function
+  ob_ajax.onreadystatechange = function(){
+    if (ob_ajax.readyState == 4) callback(ob_ajax.responseText);
+  }
+}
+//register click on #btn_cnvimg to get and save image
+
+var btn_cnvimg = document.getElementById('btn');
+if(btn_cnvimg) btn_cnvimg.addEventListener('click', function(e){
+  var imgname = window.prompt('Зурганд заавал нэр өгнө үү!\n- Хэрвээ зурганд өгсөн нэр өмнөх зургийн нэртэй ижил байвал,\n өмнөх зураг устах болохыг анхаарна уу! \n', '');
+  if(imgname !== null){
+    //set data that will be send with ajaxSend() to php (base64 PNG image-data of the canvas, and image-name)
+    zAxis = document.getElementById("z-axis").value;
+    //-----------------------traditionally = 0 or fast method  = 1---------------------
+    // 
+    var method = document.forms[0];
+    var txt = "";
+    for (var i = 0; i < method.length; i++) {
+        if (method[i].checked) {
+            txt = txt + method[i].value;
+        }
+    }
+    axis_data = imgname + " " + zAxis + " " + txt;
+    //---------------------------------------------------------------------------
+    var img_data = {'cnvimg':cnv.toDataURL('image/png', 1.0), 'imgname':imgname, 'axis': axis_data};
+    //send image-data to php file
+    ajaxSend(img_data, php_file, 'post', function(resp){
+      //show server response in #ajaxresp, if not exist, alert response
+      if(document.getElementById('ajaxresp')) document.getElementById('ajaxresp').innerHTML = resp;
+      else window.alert('Амжилттай илгээгдлээ!');
+    });
+  }
+});
---
+++ touch.php
@@ -0,0 +1,135 @@
+<html>
+<head>
+<title>Sketchpad</title>
+    <script type="text/javascript" src="js/touchdrawing.js"></script>
+    <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="css/bootstrap.css" type="text/css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/csstyle.css">
+    <script src="js/jscolor.js"></script>
+<style>
+/* Some CSS styling */
+ .navbar {
+      margin-bottom: 0;
+      border-radius: 0;
+    }
+    
+    /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
+    .row.content {height: 660px}
+    /* On small screens, set height to 'auto' for sidenav and grid */
+    @media screen and (max-width: 767px) {
+      .sidenav {
+        height: auto;
+        padding: 15px;
+      }
+      .row.content {height:auto;} 
+    }
+</style>
+</head>
+
+<body onload="init()">
+    <nav class="navbar navbar-inverse">
+
+      <div class="container-fluid">
+
+        <div class="navbar-header">
+
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>                        
+          </button>
+
+          <a href="#" id="btn" class="navbar-brand"  style="color:white; background:#4CAF50; margin-left: 10px; width: 100px; height: 40px; padding:10px 25px; margin-top: 5px;">Илгээх</a>
+          <a href="#" class="navbar-brand" onclick="clearCanvas(canvas,ctx);" style="color:white; background:#ff3333; margin-left: 10px; width: 100px; height: 40px; padding:10px 15px; margin-top: 5px;"><center>Арилгах</center></a>
+            <a href="#" style=" margin-left: 10px; width: 100px; height: 40px; padding:10px 15px; margin-top: 15px; text-decoration: none;">
+                <img src="icons/Fast_text.png" class = "icon-text" style="color:white; margin-top: 15px;" width="19px" height="19px" id="Pen">
+            </a>
+            <a href="#" style=" width: 100px; height: 40px; padding:10px 5px; margin-top: 15px; text-decoration: none;">
+                <img src="icons/draw.png" class = "icon-text" style="color:white;margin-top: 15px" width="19px" height="19px" id="draw">
+            </a>
+        </div>
+
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav" style = "text-align:center">
+                    <li class="dropdowns"><a href="#">Өнгө:
+                        <input class="jscolor" value="dd3131" id = "pen_color" name="fname"></a>
+                    </li>
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" id = "sizes" data-toggle="dropdown" href="#"><span class="caret"></span></a>
+                          <ul class="dropdown-menu" id = "myList" style = "text-align:center">
+                            <!--<li><input class="size" value="1" id = "draw_size" name="fname" type="number"></li>-->
+                                <li id="drawCanvas"><a href="#">
+                                    <canvas id="myCanvas1" width="160" height="50"></canvas></a>
+                                </li>
+                                <li id="drawCanvas"><a href="#">
+                                    <canvas id="myCanvas2" width="160" height="50"></canvas></a>
+                                </li>
+                                <li id="drawCanvas"><a href="#">
+                                    <canvas id="myCanvas3" width="160" height="50"></canvas></a>
+                                </li>
+                                <li id="drawCanvas"><a href="#">
+                                    <canvas id="myCanvas4" width="160" height="50"></canvas></a>
+                                </li>
+                          </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" id = "text_size1" data-toggle="dropdown" href="#"><span class="caret"></span></a>
+                          <ul class="dropdown-menu" id = "myList" style = "text-align:center">
+                            <!--<li><input class="size" value="1" id = "draw_size" name="fname" type="number"></li>-->
+                                <li id="textCanvas"><a href="#">
+                                    <canvas id="text1" width="160" height="50"></canvas></a>
+                                </li>
+                                <li id="textCanvas"><a href="#">
+                                    <canvas id="text2" width="160" height="50"></canvas></a>
+                                </li>
+                                <li id="textCanvas"><a href="#">
+                                    <canvas id="text3" width="160" height="50"></canvas></a>
+                                </li>
+                                <li id="textCanvas"><a href="#">
+                                    <canvas id="text4" width="160" height="50"></canvas></a>
+                                </li>
+                          </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" id = "draw_image" data-toggle="dropdown" href="#"><span class="caret"></span></a>
+                          <ul class="dropdown-menu" id = "myList">
+                            <!--<li><input class="size" value="1" id = "draw_size" name="fname" type="number"></li>-->
+                            <center>
+                            <input type="file" id="input" width="150" height="50"></center>
+
+                          </ul>
+                    </li>
+                    <li style="color: #b3b3b3; font-size: 12px; margin-left: 10px">X:<input type="number" onchange="canvas.width = document.getElementById('x-axis').value;" id = "x-axis" name="fname" value="1" style="width: 50px; margin-top:15px; color: black; text-align: center;">
+                    </li>
+                    <li style="color: #b3b3b3; font-size: 12px; margin-left: 10px">Y:<input type="number" onchange="canvas.height = document.getElementById('y-axis').value;" id = "y-axis" name="fname" value="1" style="width: 50px; margin-top:15px; color: black;text-align: center;">
+                    </li>
+                    <li style="color: #b3b3b3; font-size: 12px; margin-left: 10px">Z:<input type="number" id = "z-axis" name="fname" value="1" style="width: 50px; margin-top:15px; color: black;text-align: center;">
+                    </li>
+                </ul>
+
+                <ul class="nav navbar-nav navbar-right" style="margin-top: 1px;">
+                    <li style="color: #b3b3b3; font-size: 13px; margin-left: 2px; text-align:center;"><form>
+                      Хурдан арга <input type="radio" name="coffee" value="1" checked><br>
+                      Уламжлалт арга <input type="radio" name="coffee" value="0">
+                    </form>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div id="sketchpadapp" class="col-sm-12 col-xs-12 container">
+        <center>
+            <canvas id="sketchpad" height="400px" width="400px"> </canvas>
+        </center>
+    </div>
+</body>
+    <script>
+    function setTextColor(picker) {
+        document.getElementsByTagName('body')[0].style.color = '#' + picker.toString()
+    }
+    </script>
+ <script type="text/javascript" src="js/savings.js"></script>
+</html>
