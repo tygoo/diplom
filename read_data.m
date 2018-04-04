925b721077bf0ad8f287f27ce7783ef41e9ab1be
tygoo <37063996+tygoo@users.noreply.github.com>
2018-04-04 09:18:25 +0800

    fixed time delay

--- read_data.m
+++ read_data.m
@@ -1,4 +1,5 @@
 %folder-n file uudiin listiig todorhoiloh
+clear;
 cd ('C:\xampp\htdocs\touch\searcher');
  folderContents = dir;
  folderContents = folderContents(~ismember({folderContents.name}, {'.', '..'}));
@@ -31,17 +32,17 @@ if str2double(method) == 0
     A = imread(char(direction)); 
     figure(1);
     imshow(A);
-    Pl=5; g=12.5; zz=str2double(z_axis);   % 10x10 lens array
-    pd=0.18;
+    Pl=5; g=15; zz=str2double(z_axis);   % 10x10 lens array
+    pd=0.284;
     pi=(pd*zz)/g;
     [rows,columns,z]=size(A);
     tic
     for i=1:rows
         for j=1:columns
     %       if A(i,j,:)~=0
-            for x=1:10
+            for x=1:15
                 count = 0;
-                for y=1:10
+                for y=1:15
                     rr=i*pi; %1pixel  0.18
                     YY=(Pl*(y-0.5)-((g/zz)*(rr-Pl*(y-0.5))));
                     cc=j*pi;
@@ -64,19 +65,19 @@ if str2double(method) == 0
     t=toc;
     figure(3);
     imshow(B);
-    imwrite(B, 'C:\xampp\htdocs\touch\created_image\elemental_image_rec_tra1.bmp');
+    imwrite(B, 'C:\xampp\htdocs\touch\created_image_1\elemental_image_rec_con2.bmp');
 
 else
     
      %%%%%% niilmel dursen delgetsiin huvid shine argaar tegsh onstogt lesniii elemental image hurdan
     %%%%%% uusgeh 
     %% imput parameters
-    PD=0.18;   % pitch of display [mm]
-    g=12.5;             % gap [mm]
-    W=50;               % II image size [mm]
-    H=50;               % II image size [mm]
+    PD=0.284;   % pitch of display [mm]
+    g1=15;             % gap [mm]
+    W=75;               % II image size [mm]
+    H=75;               % II image size [mm]
     z=str2double(z_axis); % Delgetsees CDP havtgai hurtleh zai
-    PI=PD*z/g ;         % pixel size of image
+    PI1=PD*z/g1 ;         % pixel size of image
     pl=5;
     IN = imread(char(direction));
     figure(1);
@@ -86,47 +87,17 @@ else
     J=round(H/pl);
     I=round(W/pl);
 
-    %% lens tov oloh
-    tic
-    for j=0:J
-        ym=pl*((j+1)-0.5);
-        for i=0:I
-             xm=(pl*((i+1)-0.5));
-             cen_Y(j+1,i+1)=ym;
-             cen_X(j+1,i+1)=xm;
-
-        end 
-    end 
-
-
-    clear  xm ym
-
-    t = toc
-    %%
-    tic     
+    %% 
     [h w col]=size(IN);
     EI=zeros(round(H/PD),round(W/PD),3);
     EI = uint8(EI);
-
+    tic    
         for y=1:round(H/PD)
+            yc = pl *((fix((y*PD)/pl)+1)-0.5);
             for x=1:round(W/PD)
-
-    %********** start ************ toviig todorhoilno tuhain tsegtei hamgiin oir **************************        
-
-                temp_y=cen_Y-y*PD;
-                temp_x=cen_X-x*PD;
-
-                temp=(temp_x.^2+temp_y.^2).^0.5;
-                [a b]=find(temp==min(min(temp)));
-
-                        yc=cen_Y(a(1,1),b(1,1));
-                        xc=cen_X(a(1,1),b(1,1));
-
-    %********** start ********** EI->II calculate  ************************** 
-
-                        u=round((xc+(xc-x*PD)*(z/g))*(1/PI));
-                        v=round((yc+(yc-y*PD)*(z/g))*(1/PI));
-
+                 xc = pl *((fix((x*PD)/pl)+1)-0.5);
+                        u=round((xc+(xc-x*PD)*(z/g1))*(1/PI1));
+                        v=round((yc+(yc-y*PD)*(z/g1))*(1/PI1));
                         if v>=1
                             if u>=1
                                 if v<=h 
@@ -140,12 +111,8 @@ else
                         end       
                 end
         end
-
-
     t = toc
     figure(1);
     imshow(uint8(EI));
-    imwrite(EI, 'C:\xampp\htdocs\touch\created_image\elemental_image_rec_tra1.bmp');
+    imwrite(EI, 'C:\xampp\htdocs\touch\created_image_1\elemental_image_rec_fast2.bmp');
 end
-
-
